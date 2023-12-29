ac3d315b48e789287c8b675f78f592560d4a3af9
Jan <1066171532@qq.com>
2023-12-29 15:19:59 +0800

    init project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @Author: Zeng Jian
+ * @DateTime: 2023-12-19 14:58 PM
+ * @Description: Library Management	System Entrance
+ */
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
