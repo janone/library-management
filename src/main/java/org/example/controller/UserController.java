c548090b0519f361401f20b4b187ef7ff3639a8c
Jan <jian1.zeng@tcl.com>
2023-12-30 18:01:46 +0800

    when an api throw an exception then I should set a global exception handler. I am going to use jdk dynamic proxy to do that

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,7 +1,22 @@
 package org.example.controller;
 
+import org.example.common.Result;
+import org.example.entity.User;
+import org.example.service.UserService;
+
 public class UserController {
 
+    private UserService userService = new UserService();
 
+    public Result<Boolean> register(User user){
+        if(user.getAccount() == null || user.getAccount().equals("")){
+            return Result.fail("user account can not be empty");
+        }
+        if(user.getPassword()== null || user.getPassword().equals("")){
+            return Result.fail("password can not be empty");
+        }
+
+        return Result.success();
+    }
 
 }
